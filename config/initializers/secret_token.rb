cceda3f07e2146fe01a284b8a5f4d07ef3b7c750
Michael Lee Vazquez <magnus.nothus@gmail.com>
2013-04-09 10:56:01 -0700

    Initial App setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bowling::Application.config.secret_token = 'b931869ae1be0264927f5cbedddf6d59384ef1603bc4c27c32b72f7c241d0b2d727804b8d4273b9644e7171832d7ca9357b6229004be8f9b3f0d56bcb82f0022'
